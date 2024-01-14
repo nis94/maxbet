623e169fa13422b50bab8e1f132907643838e81a
nirbarlevav <nir.barlevav@shieldfc.com>
2024-01-14 23:44:16 +0200

    add prometheus & graphana

--- DictionaryWorker/src/main/java/com/maxback/dictionaryworker/controller/LengthStateController.java
+++ DictionaryWorker/src/main/java/com/maxback/dictionaryworker/controller/LengthStateController.java
@@ -1,6 +1,5 @@
 package com.maxback.dictionaryworker.controller;
 
-import com.maxback.dictionaryworker.service.DictionaryService;
 import com.maxback.dictionaryworker.service.WordService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
--- DictionaryWorker/src/main/java/com/maxback/dictionaryworker/service/WordService.java
+++ DictionaryWorker/src/main/java/com/maxback/dictionaryworker/service/WordService.java
@@ -3,7 +3,6 @@ package com.maxback.dictionaryworker.service;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
